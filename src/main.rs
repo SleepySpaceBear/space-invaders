e2653bac47ec4171e4a2a564bef2042f29476f3a
Bear <sleepyspacedad@gmail.com>
2024-03-25 10:22:59 -0400

    Added default files

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+#[allow(non_camel_case_types)]
+
+enum InputPorts {
+    INP0 = 0,
+    INP1 = 1,
+    INP2 = 2,
+    SHFT_IN = 3
+}
+
+enum OutputPorts {
+    SHFTAMNT = 2,
+    SOUND1 = 3,
+    SHFT_DATA = 4,
+    SOUND2 = 5,
+    WATCHDOG = 6
+}
+
+fn main() {
+    println!("Hello, world!");
+}
